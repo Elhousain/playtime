81076974d18e1fb2d96cd2f30a2686067504107f
Tom <s0200162@thomasmore.student.be>
2021-01-17 22:49:21 +0100

    indien ingelogd, /login naar index laten gaan

--- src/main/java/be/thomasmore/graduaten/playtime/controller/LoginController.java
+++ src/main/java/be/thomasmore/graduaten/playtime/controller/LoginController.java
@@ -1,14 +1,24 @@
 package be.thomasmore.graduaten.playtime.controller;
 
+import be.thomasmore.graduaten.playtime.entity.MyUserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
-    public String login(){
-        return "login";
+    public String login(@AuthenticationPrincipal MyUserDetails userDetails){
+        String mail="";
+        try{
+            mail= userDetails.getUsername();
+        } catch (Exception e){
+        }
+        if (mail==""){
+            return "login";
+        } else {
+            return "index";
+        }
     }
-
 }
+
